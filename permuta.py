7e116f48715883b34dfa3b4cc365a4182e797427
Osvaldo da Silva Santos <37909080+oswaldin@users.noreply.github.com>
2019-09-21 03:24:29 -0300

    Update permuta.py

--- permuta.py
+++ permuta.py
@@ -1,38 +1,24 @@
-def letras(s):
-    a = []
-    for l in s:
-        a.append(l)
-    return a
-
 def permuta(l,p):     
     if (l == []):
         return p
     else:
-        r = []
-        np = []
+        r = []        
         for c in l:            
             m = l + []          
-            if (p==[]):
-                w = '' + c
-                m.remove(c)                            
-                np.append(w)                
-                r = r + permuta(m,[w])                     
-            else:
-                for w in p:
-                    w = w + c + ''                    
-                    m.remove(c)
-                    np.append(w)           
-                    r = r + permuta(m,[w])    
+            for w in p:
+                w = w + c + ''                    
+                m.remove(c)                
+                r = r + permuta(m,[w])    
     return r
    
 def limpa(a):
     for e in a:
         if a.count(e) > 1:
-            a.remove(e)
+            for x in range(a.count(e)-1): a.remove(e)
     return a
 
 s = input('Palavra para permutação: ')
-pmt = permuta(letras(s),[])
+pmt = permuta(list(s),[''])
 pmt = limpa(pmt)
 print('================================\n', len(pmt), 'anagramas possíveis\n================================')
 print(pmt)
